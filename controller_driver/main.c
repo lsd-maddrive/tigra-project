a6349da9e36f0e28b66fb6aeeed357bce03d06c2
Алексей <darklexi494@gmail.com>
2018-05-03 23:00:47 +0300

    [common]: Added controller driver project

---
+++ controller_driver/main.c
@@ -0,0 +1,28 @@
+#include <ch.h>
+#include <hal.h>
+
+#include <chprintf.h>
+
+static THD_WORKING_AREA(waThread, 128);
+static THD_FUNCTION(Thread, arg) 
+{
+    arg = arg;
+
+    while (true)
+    {
+        chThdSleepSeconds(1);
+    }
+}
+
+int main(void)
+{
+    chSysInit();
+    halInit();
+
+    chThdCreateStatic(waThread, sizeof(waThread), NORMALPRIO, Thread, NULL /* arg is NULL */);
+
+    while (true)
+    {
+        chThdSleepSeconds(1);
+    }
+}
