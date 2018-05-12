a18bc51ebbb26929b6942c688e7b8b34af640c15
Алексей <darklexi494@gmail.com>
2018-05-12 07:18:01 +0300

    [common]: routine selection revrted to defines (from enum)

--- controller_driver/include/common.h
+++ controller_driver/include/common.h
@@ -7,13 +7,16 @@
 /*** Configuration ***/
 
 /* Program routine selection */
-typedef enum {
-	PROGRAM_ROUTINE_MASTER,
-	PROGRAM_ROUTINE_TEST_BREAK_SENSOR
+/*
+ * Bad idea to make it <enum> as we use #if (preprocessor) to
+ * check what routine to use
+ * <enum> is compilation type, not preprocessor
+ * This must be #define
+ */
+#define     PROGRAM_ROUTINE_MASTER                  0
+#define     PROGRAM_ROUTINE_TEST_BREAK_SENSOR       1
 
-} program_routine_t;
-
-#define MAIN_PROGRAM_ROUTINE				PROGRAM_ROUTINE_MASTER
+#define     MAIN_PROGRAM_ROUTINE				    PROGRAM_ROUTINE_MASTER
 
 /*** Prototypes ***/
 
