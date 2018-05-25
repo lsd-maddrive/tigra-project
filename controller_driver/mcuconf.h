bcfe15b07a7e2c03010b84541a77650b9ed28d29
Lena <seryhelena@gmail.com>
2018-05-25 19:00:05 +0300

    [lld_steer_position] add lld_steer_position.h, need fixes

--- controller_driver/mcuconf.h
+++ controller_driver/mcuconf.h
@@ -101,7 +101,7 @@
  * ADC driver system settings.
  */
 #define STM32_ADC_ADCPRE                    ADC_CCR_ADCPRE_DIV4
-#define STM32_ADC_USE_ADC1                  FALSE
+#define STM32_ADC_USE_ADC1                  TRUE
 #define STM32_ADC_USE_ADC2                  TRUE
 #define STM32_ADC_USE_ADC3                  FALSE
 #define STM32_ADC_ADC1_DMA_STREAM           STM32_DMA_STREAM_ID(2, 4)
@@ -160,7 +160,7 @@
  * GPT driver system settings.
  */
 #define STM32_GPT_USE_TIM1                  FALSE
-#define STM32_GPT_USE_TIM2                  FALSE
+#define STM32_GPT_USE_TIM2                  TRUE
 #define STM32_GPT_USE_TIM3                  FALSE
 #define STM32_GPT_USE_TIM4                  TRUE
 #define STM32_GPT_USE_TIM5                  FALSE
--- controller_driver/src/lld_steer_position.c
+++ controller_driver/src/lld_steer_position.c
@@ -1,58 +1,78 @@
-/*===========================================================================*/
+#include <lld_steer_position.h>
+
+
 /* ADC driver related.                                                       */
 /*===========================================================================*/
 
-#define ADC1_NUM_CHANNELS   2
-#define ADC1_BUF_DEPTH      1
+#define adc1NumChannels   1
+#define adc1BufDepth      1
 
-static adcsample_t samples1[ADC1_NUM_CHANNELS * ADC1_BUF_DEPTH];
+static adcsample_t samples1[adc1NumChannels * adc1BufDepth];
+
+static ADCDriver                 *adcSteerSensor    = &ADCD2;
+static GPTDriver                 *adcGPT            = &GPTD2;
+static GPTNameFunc               *adcGPTfunc        = &gpt2cfg1;
+
+
+uint16_t lldAdcSteerSensorVal = 0;
 
 /*
  * ADC streaming callback.
  */
 
-// when ADC conversion ends, this func will be called
-/* if the depth is equal to 1, another way it will be called twice per conversion!
-   conversion is sampling of all your cnahhel N times (N is buffer depth) */
-static void adccallback(ADCDriver *adcp, adcsample_t *buffer, size_t n)
+static void adc1cb(ADCDriver *adcp, adcsample_t *buffer, size_t n)
 {
-  (void)adcp;
+    adcp = adcp; n = n;
 
-  chSysLockFromISR();                   // Critical Area
-  chMBPostI( &test_mb, samples1[0]);    // send 1st ADC-value (channel 1)
-  chMBPostI( &test_mb, samples1[1]);    // send 2nd ADC-value (channel 2)
-  chSysUnlockFromISR();                 // Close Critical Area
+    lldAdcSteerSensorVal = samples1[0];
+//    chSysLockFromISR();                   // Critical Area
+//    chMBPostI( &test_mb, samples1[0]);    // send 1st ADC-value (channel 1)
+//    chSysUnlockFromISR();                 // Close Critical Area
 
 }
 
-/*
- * ADC errors callback, should never happen.
- */
-static void adcerrorcallback(ADCDriver *adcp, adcerror_t err) {
+static void adc1Errorcb(ADCDriver *adcp, adcerror_t err) {
 
-  (void)adcp;
-  (void)err;
+    adcp = adcp; err = err;
+
 }
 
-static const ADCConversionGroup adcgrpcfg1 = {
+static const ADCConversionGroup adccfg1 = {
   .circular     = true,                     // working mode = looped
-  /* Buffer will continue writing to the beginning when it come to the end */
-  .num_channels = ADC1_NUM_CHANNELS,    // number of channels
-  .end_cb       = adccallback,              // after ADC conversion ends - call this func
-  /* Don`t forget about depth of buffer */
-  .error_cb     = adcerrorcallback,         // in case of errors, this func will be called
-  .cr1          = 0,                        // just because it has to be 0
-  /* Cause we don`t need to write something to the register */
-  .cr2          = ADC_CR2_EXTEN_RISING | ADC_CR2_EXTSEL_SRC(12),  // Commutated from GPT
-  /* 12 means 0b1100, and from RM (p.449) it is GPT4 */
+  .num_channels = adc1NumChannels,          // number of channels
+  .end_cb       = adc1cb,                   // after ADC conversion ends - call this func
+  .error_cb     = adc1Errorcb,
+  .cr1          = 0,
+  .cr2          = ADC_CR2_EXTEN_RISING | ADC_CR2_EXTSEL_SRC(11),  // Commutated from GPT
+  /* 11 means 0b1011, and from RM (p.452) it is GPT2 */
   /* ADC_CR2_EXTEN_RISING - means to react on the rising signal (front) */
   .smpr1        = ADC_SMPR1_SMP_AN10(ADC_SAMPLE_144),       // for AN10 - 144 samples
-  .smpr2        = ADC_SMPR2_SMP_AN3(ADC_SAMPLE_144),        // for AN3  - 144 samples
-  .sqr1         = ADC_SQR1_NUM_CH(ADC1_NUM_CHANNELS),   //
-  /* Usually this field is set to 0 as config already know the number of channels (.num_channels) */
+  .sqr1         = ADC_SQR1_NUM_CH(adc1NumChannels),
   .sqr2         = 0,
-  .sqr3         = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN3) |         // sequence of channels
-                  ADC_SQR3_SQ2_N(ADC_CHANNEL_IN10)
-  /* If we can macro ADC_SQR2_SQ... we need to write to .sqr2 */
+  .sqr3         = ADC_SQR3_SQ2_N(ADC_CHANNEL_IN10)
 };
+
+
+/*
+ * GPT2 configuration. This timer is used as trigger for the ADC.
+ */
+static const GPTConfig gpt2cfg1 = {
+  .frequency =  100000,
+  .callback  =  NULL,
+  .cr2       =  TIM_CR2_MMS_1,  /* MMS = 010 = TRGO on Update Event.        */
+  .dier      =  0U
+};
+
+/*
+ * @brief                   Initialize periphery connected to steering sensor
+ */
+void lldSteerPositionInit( void )
+{
+    adcStart( adcSteerSensor, NULL );
+    palSetLineMode( LINE_ADC123_IN10, PAL_MODE_INPUT_ANALOG );  // PC0
+    adcStartConversion( adcSteerSensor, &adccfg1, samples1, adc1BufDepth);
+
+    gptStart( adcGPT, adcGPTfunc );
+    gptStartContinuous( adcGPT, gpt2cfg1.frequency/1000);
+}
 
