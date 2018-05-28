2fa4b6c1699a6a12d1440dc1ab4bfcc7c3c4e8fc
Алексей <darklexi494@gmail.com>
2018-05-29 00:15:12 +0300

    [common]: new project.mk features

--- controller_driver/project.mk
+++ controller_driver/project.mk
@@ -1,16 +1,19 @@
 
-PROJECT_MODULES = src/lld_brake_sensor.c 		\
-				  src/brake_unit_cs.c 			\
-				  src/lld_clutch_lever.c 		\
-				  src/lld_control.c 			\
-				  src/lld_wheel_pos_sensor.c 
+PROJECT_MODULES = lld_brake_sensor.c 		\
+				  brake_unit_cs.c 			\
+				  lld_clutch_lever.c 		\
+				  lld_control.c 			\
+				  lld_wheel_pos_sensor.c 
 				  
-PROJECT_TESTS	= tests/test_brake_sensor.c 	\
-				  tests/test_brake_unit_cs.c 	\
-				  tests/test_clutch_lever.c 	\
-				  tests/test_lld_control.c 		\
-				  tests/test_wheel_pos_sensor.c
+PROJECT_MODULES := $(addprefix src/,$(PROJECT_MODULES))
+				  
+PROJECT_TESTS	= test_brake_sensor.c 		\
+				  test_brake_unit_cs.c 		\
+				  test_clutch_lever.c 		\
+				  test_lld_control.c 		\
+				  test_wheel_pos_sensor.c
 
+PROJECT_TESTS := $(addprefix tests/,$(PROJECT_TESTS))
 
 PROJECT_CSRC 	= src/main.c src/common.c \
 					$(PROJECT_MODULES) $(PROJECT_TESTS)
