11559d7d055356431154e7914a08e5af451f9156
Lena <seryhelena@gmail.com>
2018-06-14 13:13:41 +0300

    [dr_sonars] Try to use UART for sensors, not working

--- controller_driver/mcuconf.h
+++ controller_driver/mcuconf.h
@@ -277,7 +277,7 @@
 #define STM32_SERIAL_USE_USART2             FALSE
 #define STM32_SERIAL_USE_USART3             FALSE
 #define STM32_SERIAL_USE_UART4              FALSE
-#define STM32_SERIAL_USE_UART5              FALSE
+#define STM32_SERIAL_USE_UART5              TRUE
 #define STM32_SERIAL_USE_USART6             FALSE
 #define STM32_SERIAL_USE_UART7              TRUE
 #define STM32_SERIAL_USE_UART8              FALSE
--- controller_driver/tests/test_lld_sonars.c
+++ controller_driver/tests/test_lld_sonars.c
@@ -8,6 +8,13 @@
 
 static const SerialConfig sdcfg = {
   .speed = 115200,
+  .cr1 = (!(USART_CR1_PS | USART_CR1_M | USART_CR1_M_0)),
+  .cr2 = USART_CR2_LINEN,
+  .cr3 = 0
+};
+
+static const SerialConfig sd5cfg = {
+  .speed = 9600,
   .cr1 = 0,
   .cr2 = USART_CR2_LINEN,
   .cr3 = 0
@@ -26,20 +33,36 @@ void testSonarsRoutineWorking( void )
     palSetPadMode( GPIOE, 8, PAL_MODE_ALTERNATE(8) );    // TX
     palSetPadMode( GPIOE, 7, PAL_MODE_ALTERNATE(8) );    // RX
 
+    sdStart( &SD5, &sd5cfg );
+    palSetPadMode( GPIOC, 12, PAL_MODE_ALTERNATE(8) );    // TX
+    palSetPadMode( GPIOD,  2, PAL_MODE_ALTERNATE(8) );    // RX
+
+
     uint16_t adcSonarVal = 0;
     uint16_t adcSonarVal2 = 0;
-
+    uint8_t test_uart = 0;
     lldSonarSync();
+//    USART_CR1_PS
+//    sdcfg.Init.Parity =  USART_PARITY_NONE;
+//    chprintf( (BaseSequentialStream *)&SD7, "testtttttt\n\r");
     while( true )
     {
 
+//      chprintf( (BaseSequentialStream *)&SD5, "test\n\r");
+
+      test_uart = sdGet(&SD5);
+      sdWrite(&SD7, (uint8_t*)&test_uart, sizeof(test_uart));
+
+
+//      chprintf( (BaseSequentialStream *)&SD7, "test: %d\n\r", test_uart);
 
 //        msg_t msg = sdGetTimeout( &SD7, MS2ST( 10 ) );
-        adcSonarVal  = lldSonar7077AdcVal(1);
-        adcSonarVal2 = lldSonar7077AdcVal(2);
+//        adcSonarVal  = lldSonar7077AdcVal(1);
+//        adcSonarVal2 = lldSonar7077AdcVal(2);
+//        chprintf( (BaseSequentialStream *)&SD5, "S1: %d\n\r", adcSonarVal2);
 //        sdWrite(&SD7, (uint8_t*)&adcSonarVal, sizeof(adcSonarVal));
 //        chprintf((BaseSequentialStream *)&SD7, "Hi\n\r");
-        chprintf( (BaseSequentialStream *)&SD7, "S1: %d   S2: %d\n\r", adcSonarVal, adcSonarVal2);
+//        chprintf( (BaseSequentialStream *)&SD7, "S1: %d   S2: %d\n\r", adcSonarVal, adcSonarVal2);
 //        chprintf( (BaseSequentialStream *)&SD7, "UART %d", msg);
 
         chThdSleepMilliseconds( 100 );
