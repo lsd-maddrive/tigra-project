84794817d5ebdd07bfa9b040e570e4b9bebe093b
AlexSAU <alex_sau@etu.ru>
2018-06-19 19:47:28 +0300

    [ros]: stereo calibration new features

---
+++ ros/calib_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r stereo_camera_info ~/.ros
--- ros/calib_start.sh
+++ ros/calib_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rosrun camera_calibration cameracalibrator.py \
-	--size 9x7 --square 0.023 \
+	--size 7x5 --square 0.038 \
 	right:=/stereo/right/image_raw left:=/stereo/left/image_raw \
 	right_camera:=/stereo/right left_camera:=/stereo/left  \
 	--no-service-check --approximate=0.01
