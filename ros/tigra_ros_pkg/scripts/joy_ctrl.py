7cca1d13392a15cb8f9400b305007b295e8b0062
Alex (quadro) <alex_quadro@etu.ru>
2018-08-16 18:17:57 +0300

    [ros]: Created ros package

--- ros/tigra_ros_pkg/scripts/joy_ctrl.py
+++ ros/tigra_ros_pkg/scripts/joy_ctrl.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 import actionlib
 from geometry_msgs.msg import Twist
@@ -10,41 +12,90 @@ import threading
 
 rospy.init_node('control_link')
 
+debug_enabled = rospy.get_param('~debug', False)
+if debug_enabled:
+    print('Debug enabled')
+
 steer_value = 0
 speed_value = 0
 current_mode = 0
+
+# 0 - Blue
+# 1 - Green
+# 2 - Red
+# 3 - Yellow
+# 4 - LU
+# 5 - RU
+# 6 - LB
+# 7 - RB
+# 10 - Left stick
+# 11 - Right stick
+
+button_names = ['Blue', 
+                'Green',
+                'Red',
+                'Yellow',
+                'Left upper',
+                'Right upper',
+                'Left bottom',
+                'Right bottom',
+                '_',
+                '_',
+                'Left stick',
+                'Right stick'
+                ]
+
+axes_names = [  'Lstick horz',
+                'Lstick vert',
+                'Rstick horz',
+                'Rstick vert',
+                'Btns horz',
+                'Btns vert'
+                ]
+
+def show_clicked(msg):
+    print('Buttons:')
+    for i in range(len(msg.buttons)):
+        if msg.buttons[i] != 0:
+            print('\t' + button_names[i])
+
+    print('Axes:')
+    for i in range(len(msg.axes)):
+        print('\t%s: %.2f' % (axes_names[i], msg.axes[i]))
 
 
 def joy_cb(msg):
     global steer_value, speed_value, current_mode
 
+    if debug_enabled:
+        show_clicked(msg)
+        
     # print(msg)
 
+
+
+
     if msg.buttons[0]:
-        print('X (blue) pressed')
         current_mode = 0
         mode_pub.publish( current_mode ) 
 
     if msg.buttons[1]:
-        print('A (green) pressed')
+        pass
 
     if msg.buttons[6] and msg.buttons[7]:
         if msg.buttons[2]:
-            print('B (red) pressed')
             # Start
             current_mode = 2
             mode_pub.publish( current_mode ) 
 
 
         if msg.buttons[3]:
-            print('Y (yellow) pressed')
             # Prepare
             current_mode = 1
             mode_pub.publish( current_mode ) 
 
 
     if msg.buttons[4]:
-        print('LB pressed')
         steer_value = 0
 
     if msg.axes[0] != 0:
@@ -63,7 +114,7 @@ def joy_cb(msg):
 
     # cmd = [msg.linear.x * 100, msg.angular.z * -150]
 
-    print('Cmd: ', [speed_value, steer_value])
+    # print('Cmd: ', [speed_value, steer_value])
 
 
 rospy.Subscriber('joy', Joy, joy_cb, queue_size = 1)
