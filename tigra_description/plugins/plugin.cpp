b8f4eb9a8a8f526cfa87f82f701c0ddc38a79a0d
maryrosened <kushnarev.nikita99@mail.ru>
2021-02-28 12:12:31 +0300

    Update plugin

--- tigra_description/plugins/plugin.cpp
+++ tigra_description/plugins/plugin.cpp
@@ -120,6 +120,40 @@ void TigraPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
 
     sub_vel_cmd_ = n_.subscribe("cmd_vel", 1, &TigraPlugin::onCmdVel, this);
 
+/* Prepare publishers */
+    double pose_cov_list[6] = { 0.001, 0.001, 0.001, 0.001, 0.001, 0.03 }; 
+    double twist_cov_list[6] = { 0.001, 0.001, 0.001, 0.001, 0.001, 0.03 }; 
+
+    // Setup odometry realtime publisher + odom message constant fields
+    odom_pub_.reset(new realtime_tools::RealtimePublisher<nav_msgs::Odometry>(n_, "odom", 100));
+    odom_pub_->msg_.header.frame_id = odom_frame_id_;
+    odom_pub_->msg_.child_frame_id = base_frame_id_;
+    odom_pub_->msg_.pose.pose.position.z = 0;
+    // odom_pub_->msg_.pose.covariance = boost::assign::list_of
+    //                                   (static_cast<double>(pose_cov_list[0])) (0)  (0)  (0)  (0)  (0)
+    //                                   (0)  (static_cast<double>(pose_cov_list[1])) (0)  (0)  (0)  (0)
+    //                                   (0)  (0)  (static_cast<double>(pose_cov_list[2])) (0)  (0)  (0)
+    //                                   (0)  (0)  (0)  (static_cast<double>(pose_cov_list[3])) (0)  (0)
+    //                                   (0)  (0)  (0)  (0)  (static_cast<double>(pose_cov_list[4])) (0)
+    //                                   (0)  (0)  (0)  (0)  (0)  (static_cast<double>(pose_cov_list[5]));
+    odom_pub_->msg_.twist.twist.linear.y  = 0;
+    odom_pub_->msg_.twist.twist.linear.z  = 0;
+    odom_pub_->msg_.twist.twist.angular.x = 0;
+    odom_pub_->msg_.twist.twist.angular.y = 0;
+    // odom_pub_->msg_.twist.covariance = boost::assign::list_of
+    //                                    (static_cast<double>(twist_cov_list[0])) (0)  (0)  (0)  (0)  (0)
+    //                                    (0)  (static_cast<double>(twist_cov_list[1])) (0)  (0)  (0)  (0)
+    //                                    (0)  (0)  (static_cast<double>(twist_cov_list[2])) (0)  (0)  (0)
+    //                                    (0)  (0)  (0)  (static_cast<double>(twist_cov_list[3])) (0)  (0)
+    //                                    (0)  (0)  (0)  (0)  (static_cast<double>(twist_cov_list[4])) (0)
+    //                                    (0)  (0)  (0)  (0)  (0)  (static_cast<double>(twist_cov_list[5]));
+    tf_odom_pub_.reset(new realtime_tools::RealtimePublisher<tf::tfMessage>(n_, "/tf", 100));
+    tf_odom_pub_->msg_.transforms.resize(1);
+    tf_odom_pub_->msg_.transforms[0].transform.translation.z = 0.0;
+    tf_odom_pub_->msg_.transforms[0].child_frame_id = base_frame_id_;
+    tf_odom_pub_->msg_.transforms[0].header.frame_id = odom_frame_id_;
+
+
     cout << "TigraPlugin plugin loaded!" << endl;
 }
 
@@ -137,9 +171,63 @@ void TigraPlugin::OnUpdate(const common::UpdateInfo &info)
 
     // twistStateUpdate();
     updateCurrentState();
+    updateOdometry();
 
     driveUpdate();
     steeringUpdate();
+}
+
+void TigraPlugin::updateOdometry()
+{
+    const double linear = cur_virtual_speed_rps_ / mps2rps * time_step_;
+    const double angular = linear * tan(cur_virtual_steering_rad_) / wheelbase_;
+    const double curvature_radius = wheelbase_ / cos(M_PI/2.0 - cur_virtual_steering_rad_);
+
+    linear_ = linear;
+    angular_ = angular;
+
+    if (fabs(curvature_radius) > 0.0001)
+    {
+        const double elapsed_distance = linear;
+        const double elapsed_angle = elapsed_distance / curvature_radius;
+        const double x_curvature = curvature_radius * sin(elapsed_angle);
+        const double y_curvature = curvature_radius * (cos(elapsed_angle) - 1.0);
+        const double wheel_heading = yaw_ + cur_virtual_steering_rad_;
+        y_ += x_curvature * sin(wheel_heading) + y_curvature * cos(wheel_heading);
+        x_ += x_curvature * cos(wheel_heading) - y_curvature * sin(wheel_heading);
+        yaw_ += elapsed_angle;
+    }
+    
+    /*** Update publishers ***/
+    if ( last_odom_update_time_ + publish_period_ > last_update_time_ )
+    {
+        return;
+    }
+
+    last_odom_update_time_ += publish_period_;
+
+    const geometry_msgs::Quaternion orientation(tf::createQuaternionMsgFromYaw(yaw_));
+
+    if (odom_pub_->trylock())
+    {
+        odom_pub_->msg_.header.stamp = ros::Time( last_update_time_.Double() );
+        odom_pub_->msg_.pose.pose.position.x = x_ + wheelbase_ * (1.0 - cos(yaw_));
+        odom_pub_->msg_.pose.pose.position.y = y_ - wheelbase_ * sin(yaw_);
+        odom_pub_->msg_.pose.pose.orientation = orientation;
+        odom_pub_->msg_.twist.twist.linear.x  = 0;
+        odom_pub_->msg_.twist.twist.angular.z = 0;
+        odom_pub_->unlockAndPublish();
+    }
+
+    if (tf_odom_pub_->trylock())
+    {
+        geometry_msgs::TransformStamped& odom_frame = tf_odom_pub_->msg_.transforms[0];
+        odom_frame.header.stamp = ros::Time( last_update_time_.Double() );
+        odom_frame.transform.translation.x = x_ + wheelbase_ * (1.0 - cos(yaw_));
+        odom_frame.transform.translation.y = y_ - wheelbase_ * sin(yaw_);
+        odom_frame.transform.rotation = orientation;
+        tf_odom_pub_->unlockAndPublish();
+    }
 }
 
 void TigraPlugin::driveUpdate()
