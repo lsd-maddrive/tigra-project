c217bc2c99436d5deb0aecb7f4a43c94face8930
Alex Home WSL <alex_home_wsl@example.com>
2022-04-10 15:48:34 +0300

    Added adas package as a requirement

---
+++ scripts/update_adas.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# NB - not used now, poetry not work with -e option =(
+source ./venv38/bin/activate
+pip install -e git+https://github.com/lsd-maddrive/adas_system#egg=maddrive-adas
+
+
