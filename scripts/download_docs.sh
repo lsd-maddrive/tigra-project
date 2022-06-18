8346986723b90a81ae46c688eda902a082b39ef2
Alexey Devyatkin <nine-4lex@mail.ru>
2022-06-18 20:04:42 +0300

    Delete download_docs.sh

--- scripts/download_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# https://drive.google.com/file/d/1iIvuMr4xtmul_ea4DkjYoXMdhi8A7dez/view?usp=sharing
-wget -N --no-check-certificate \
-	'https://docs.google.com/uc?export=download&id=1iIvuMr4xtmul_ea4DkjYoXMdhi8A7dez' \
-	-O docs/Schemes.drawio
