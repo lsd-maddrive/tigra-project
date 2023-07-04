39c0a985f97a05596bdc2a50870b05bf5e8d98f1
5met4nka <pavilich99@mail.ru>
2023-07-04 22:52:41 +0300

    added realsense packages to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,16 +6,18 @@ export CMAKE_PREFIX_PATH=/usr/local/lib/cmake/:$CMAKE_PREFIX_PATH
 catkin build \
     tigra-project \
     ackermann_raw_controller_plugin \
-    ublox \
+    elp_stereo_camera \
     rtabmap \
     rtabmap_ros \
-    spatio_temporal_voxel_layer \
-    camera_calibration \
-    elp_stereo_camera \
-    stereo_image_proc \
+    ublox \
     cv_bridge \
+    stereo_image_proc \
+    camera_calibration \
+    spatio_temporal_voxel_layer \
     maddrive_urdf_tools \
     maddrive_teleop \
+    realsense2_camera \
+    realsense2_description \
     -j$(($(nproc)-2)) \
     --cmake-args -D OpenCV_DIR="/usr/local/lib/cmake/opencv4" -D RTABMAP_SYNC_MULTI_RGBD=ON -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
     
