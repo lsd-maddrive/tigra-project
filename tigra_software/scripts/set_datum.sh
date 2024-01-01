5cccfad3100473bcaeebe6286e5e8b3e4df03545
5met4nka <nlobazev@gmail.com>
2024-01-01 13:35:09 +0300

    some experiments with RL - no orientation data in NAMI dataset

--- tigra_software/scripts/set_datum.sh
+++ tigra_software/scripts/set_datum.sh
@@ -11,7 +11,7 @@ $(rosservice call /datum "geo_pose:
     longitude: $longitude
     altitude: $altitude
   orientation:
-    x: 0.014415429457016724
-    y: -0.010045033975473065
-    z: -0.34507838347250286
-    w: 0.9103334021823095")
+    x: 0.016
+    y: -0.008
+    z: -0.412
+    w: 0.911")
